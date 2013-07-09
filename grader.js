0abb50cb426e84763e63e1edc91bbf7baf09fc3d
Ubuntu <ubuntu@ip-172-31-32-200.eu-west-1.compute.internal>
2013-07-09 19:09:08 +0000

    update final grader.js

--- grader.js
+++ grader.js
@@ -65,7 +65,7 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), null/*HTMLFILE_DEFAULT*/)
         .option('-u, --url <url>','Url',null,null)
         .parse(process.argv);
     if(program.file){
@@ -73,8 +73,7 @@ if(require.main == module) {
 	var outJson = JSON.stringify(checkJson, null, 4);
 	console.log(outJson);
     }
-    if (program.url){
-	console.log('aqui');
+    else if (program.url){
 	    rest.get(program.url).on('complete', function(urlcontent){
 	    var checkjson=checkString(urlcontent, program.checks);
 	    var out=JSON.strigify(checkjson, null, 4);
